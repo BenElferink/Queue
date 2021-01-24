5bf6b3a039d0298bf777319cdb767475eb7cbcd2
Ben Elferink <ben.elferink@icloud.com>
2021-01-24 16:08:40 +0200

    test commit

--- client/src/Components/Home/HomeCanvas/Canvas.jsx
+++ client/src/Components/Home/HomeCanvas/Canvas.jsx
@@ -1,27 +1,37 @@
-import React, { useState } from 'react'
-import './Canvas.css'
+import React, { useState } from 'react';
+import './Canvas.css';
 import Button from '@material-ui/core/Button';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 function Canvas() {
-    const [hostNameInput, setHostNameInput] = useState("")
-    return (
-        <div className="canvas">
-            <div className="canvas__text">
-
-                <h1>Want to start a <span>Session</span>?</h1>
-                <p>You are just one click away, Enter your Name and Boom!! It's already created.</p>
-                <div className="canvas__InputField">
-                    <input type="text" value={hostNameInput} onChange={(e)=>setHostNameInput(e.target.value)} required/>
-                    <Button variant="contained" startIcon={<AddCircleIcon/>} color="primary" disabled>
-                        Host
-                    </Button>
-                </div>
-                 
-            </div>
-            
+  const [hostNameInput, setHostNameInput] = useState('');
+  return (
+    <div className='canvas'>
+      <div className='canvas__text'>
+        <h1>
+          Want to start a <span>Session</span>?
+        </h1>
+        <p>
+          You are just one click away! Just enter your name! (it's that simple)
+          <br />
+          <span>
+            <u>Disclaimer</u>: we do not collect any data!
+          </span>
+        </p>
+        <div className='canvas__InputField'>
+          <input
+            type='text'
+            value={hostNameInput}
+            onChange={(e) => setHostNameInput(e.target.value)}
+            required
+          />
+          <Button variant='contained' startIcon={<AddCircleIcon />} color='primary'>
+            Host
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Canvas
+export default Canvas;
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,50 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// api base-url (that you created on server side)
-const url = 'http://localhost:8080/';
+// // api base-url (that you created on server side)
+// const url = 'http://localhost:8080/';
+// const headers = () => {
+//   const token = localStorage.getItem('token')
+//   return {headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: 'Bearer ' + token,
+//   }
+// }}
+
+// const newSession = async (body) => {
+//   try {
+//     const response = await axios.post(`${url}/session/new`, body)
+//     console.log(response.status, response.statusText);
+//     localStorage.setItem('token', response.data.yourToken)
+//     return response.data.session;
+//   } catch (error) {
+//     console.log(error.message);
+//     return false
+//   }
+// }
+
+// const deleteSession = async (id) => {
+//   try {
+//     const response = await axios.delete(`${url}/session/${id}`, headers());
+//     console.log(response.status, response.statusText);
+//     return true
+//   } catch (error) {
+//     console.log(error.message);
+//     return false
+//   }
+// }
+
+// const moveToHistory = async (id, qid) => {
+//   try {
+//     const response = await axios.delete(`${url}/session/${id}/quest/${qid}`, headers());
+//     console.log(response.status, response.statusText);
+//     return true
+//   } catch (error) {
+//     console.log(error.message);
+//     return false
+//   }
+// }
+
+// // user routes
+// axios.get('/session/:id', requestSession);
+// axios.post('/session/:id/login', newUser);
+// axios.post('/session/:id/quest', authenticateToken, addToQueue);
