e69a42594fd2d52b4fb53af5227fc24996d9dc76
AmanKuvera-Dev <65402367+AmanKuvera-Dev@users.noreply.github.com>
2021-01-24 19:10:52 +0530

    Report Bugs
    - Report Bugs Section added on Home Page

--- client/src/Components/Home/HomeCanvas/Canvas.jsx
+++ client/src/Components/Home/HomeCanvas/Canvas.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Canvas.css'
 import Button from '@material-ui/core/Button';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 function Canvas() {
     const [hostNameInput, setHostNameInput] = useState("")
+    const [disableHostButton, setDisableHostButton] = useState(true)
+
+    useEffect(() => {
+        if(hostNameInput !== ""){
+            setDisableHostButton(false)
+        }
+        else{
+            setDisableHostButton(true)
+        }
+    }, [hostNameInput])
+
     return (
         <div className="canvas">
             <div className="canvas__text">
@@ -12,8 +23,8 @@ function Canvas() {
                 <h1>Want to start a <span>Session</span>?</h1>
                 <p>You are just one click away, Enter your Name and Boom!! It's already created.</p>
                 <div className="canvas__InputField">
-                    <input type="text" value={hostNameInput} onChange={(e)=>setHostNameInput(e.target.value)} required/>
-                    <Button variant="contained" startIcon={<AddCircleIcon/>} color="primary" disabled>
+                    <input type="text" value={hostNameInput} onChange={(e)=> setHostNameInput(e.target.value)} required/>
+                    <Button variant="contained" startIcon={<AddCircleIcon/>} color="primary" disabled={disableHostButton}>
                         Host
                     </Button>
                 </div>
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import blackQueueLogo from './../../images/blackQueueLogo.svg'
+import HomeIcon from '@material-ui/icons/Home';
+import { IconButton, Slide, Tooltip } from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
+import CodeIcon from '@material-ui/icons/Code';
+import BugReportIcon from '@material-ui/icons/BugReport';
+import { NavHashLink  } from 'react-router-hash-link';
 
 function Navbar() {
     const [navbar,setNavbar] = useState(false)
@@ -22,9 +28,38 @@ function Navbar() {
             <div className="navbar__left">
                 <img src={blackQueueLogo} className="navbar__logo" alt=""/>
             </div>
-            <div className="navbar__right">
-                
-            </div>
+            <Slide in={true} direction={"left"} timeout={500}>
+                <div className="navbar__right">
+                    <NavHashLink smooth to="/#home">
+                        <Tooltip position="bottom-end" title="Home">
+                            <IconButton>
+                                <HomeIcon/>
+                            </IconButton> 
+                        </Tooltip>
+                    </NavHashLink>
+                    <NavHashLink smooth to="#aboutQueue">
+                        <Tooltip position="bottom-end" title="About Us">
+                            <IconButton>
+                                <InfoIcon/>
+                            </IconButton> 
+                        </Tooltip>
+                    </NavHashLink>
+                    <NavHashLink smooth to="#DevelopersInfo">
+                        <Tooltip position="bottom-end" title="Developer Info">
+                            <IconButton>
+                                <CodeIcon/>
+                            </IconButton> 
+                        </Tooltip>
+                    </NavHashLink>
+                    <NavHashLink smooth to="#ContactInfo">
+                        <Tooltip position="bottom-end" title="Report Bug">
+                            <IconButton>
+                                <BugReportIcon/>
+                            </IconButton> 
+                        </Tooltip>
+                    </NavHashLink>
+                </div>
+            </Slide>
         </div>
     )
 }
