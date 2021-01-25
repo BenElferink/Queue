189178272cd76f4987b333699963a6fa36d7e4e3
Ben Elferink <ben.elferink@icloud.com>
2021-01-25 16:58:18 +0200

    refactor start-session on homepage

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import Canvas from './HomeCanvas/Canvas'
-import './Home.css'
-import HomeFeatures from './HomeFeatures/HomeFeatures'
-import Footer from './ContactFooter/Footer'
-import Donate from './Donate/Donate'
+import './Home.css';
+import StartSession from './StartSession/StartSession';
+import HomeFeatures from './HomeFeatures/HomeFeatures';
+import Donate from './Donate/Donate';
+import Footer from './ContactFooter/Footer';
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="home__canvas">
-                <Canvas/>
-            </div>
-            
-            <div className="home__whatQueue" id="aboutQueue">
-                <h1>What is Queue?</h1>
-                <p>In the modern scenario of teaching and education, due to the pandemic, teachers and mentors have been facing major issues of concern with respect to the productivity of their teaching sessions because of the irregular and poorly managed Questions (Doubts) and Answers (Solutions) asked in between the "online" sessions. Queue provides the host with various features that help them to manage and organize the session into various productive parts in a way that increases the amount of knowledge shared by the host as well as the number of doubts cleared. </p>
-            </div>
+  return (
+    <div className='home'>
+      <StartSession />
 
-            <div className="home__features">
-                <HomeFeatures/>
-            </div>
-            <Donate/>
-            <Footer/>
-        </div>
-    )
+      <div className='home__whatQueue' id='aboutQueue'>
+        <h1>What is Queue?</h1>
+        <p>
+          In the modern scenario of teaching and education, due to the pandemic, teachers and
+          mentors have been facing major issues of concern with respect to the productivity of their
+          teaching sessions because of the irregular and poorly managed Questions (Doubts) and
+          Answers (Solutions) asked in between the "online" sessions. Queue provides the host with
+          various features that help them to manage and organize the session into various productive
+          parts in a way that increases the amount of knowledge shared by the host as well as the
+          number of doubts cleared.{' '}
+        </p>
+      </div>
+
+      <div className='home__features'>
+        <HomeFeatures />
+      </div>
+      <Donate />
+      <Footer />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ client/src/Components/Home/StartSession/StartSession.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import styles from './StartSession.module.css';
+import Button from '@material-ui/core/Button';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
+// import { newSession } from '../../../api';
+
+export default function StartSession() {
+  const [hostNameInput, setHostNameInput] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // const response = await newSession({ username: hostNameInput });
+    // console.log(response);
+  };
+
+  return (
+    <div className={styles.component}>
+      <div className={styles.text}>
+        <h1>
+          Want to start a <span>Session</span>?
+        </h1>
+        <p>
+          You are just one click away! Enter your name! (it's that easy)
+          <br />
+          <span>
+            <u>Disclaimer:</u> We do not collect any data!
+          </span>
+        </p>
+      </div>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={hostNameInput}
+          onChange={(e) => setHostNameInput(e.target.value)}
+          required
+        />
+        <Button
+          variant='contained'
+          startIcon={<AddCircleIcon />}
+          color='primary'
+          type='submit'
+          disabled={hostNameInput === ''}>
+          Host
+        </Button>
+      </form>
+
+      <ExpandMoreRoundedIcon />
+    </div>
+  );
+}
