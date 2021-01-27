6a86f4481d7c910b442e9c6bd01bb8b3cd14acd8
AmanKuvera-Dev <65402367+AmanKuvera-Dev@users.noreply.github.com>
2021-01-28 01:02:22 +0530

    Update TimerSnackbar.jsx

--- client/src/Components/TimerSnackbar/TimerSnackbar.jsx
+++ client/src/Components/TimerSnackbar/TimerSnackbar.jsx
@@ -1,7 +1,8 @@
-import {Fragment, useState} from 'react'
+import {Fragment, useEffect, useState} from 'react'
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import styles from './TimerSnackbar.module.css'
+import like from "./sound-files/state-change_confirm-up.wav";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant='filled' {...props} />;
@@ -13,6 +14,17 @@ export default function TimerSnackbar({ snack, setSnack }) {
     vertical: 'top',
     horizontal: 'center',
   });
+
+  const likeAudio = new Audio(like);
+
+  const playSound = (audioFile) => {
+    audioFile.play();
+  };
+  
+  useEffect(() => {
+    playSound(likeAudio)
+    // eslint-disable-next-line
+  }, [])
 
   const { vertical, horizontal } = state;
 
