c976fea46b9128c48a6a29cedd2db50c0d066ea1
Ben Elferink <ben.elferink@icloud.com>
2021-02-01 01:13:00 +0200

    link with domain

--- client/src/Components/SessionUrl/SessionUrl.jsx
+++ client/src/Components/SessionUrl/SessionUrl.jsx
@@ -5,8 +5,8 @@ import { useRef, useState } from 'react';
 export default function SessionUrl({ roomId, closeThis }) {
   const [isCopied, setIsCopied] = useState(false);
   const copyRef = useRef();
-  // const joinUrl = `http://www.my-queue.tech/join/${roomId}`;
-  const joinUrl = `https://queue-client.herokuapp.com/join/${roomId}`;
+  const joinUrl = `http://www.my-queue.tech/join/${roomId}`;
+  // const joinUrl = `https://queue-client.herokuapp.com/join/${roomId}`;
 
   const copyOutput = () => {
     if (!isCopied) {
