b642b77e29258ece2366c0fbad4372f967bd9b16
Ben Elferink <ben.elferink@icloud.com>
2021-02-01 01:43:48 +0200

    timer bug

--- client/src/Components/Navbar/TimerModal/TimerModal.jsx
+++ client/src/Components/Navbar/TimerModal/TimerModal.jsx
@@ -13,16 +13,14 @@ export default function TimerModal({
   setTimer,
   setShowTimerModal,
 }) {
-
   const clickConfirm = () => {
-    setTimer({ minutes: selectedMinutes, seconds: 0 });
-    if(window.Notification && Notification.permission === "granted"){
+    if (window.Notification && Notification.permission === 'granted') {
+      setTimer({ minutes: selectedMinutes, seconds: 0 });
       setShowTimerModal(false);
-    }else if(window.Notification && Notification.permission !== "denied"){
-       Notification.requestPermission()
+    } else if (window.Notification && Notification.permission !== 'denied') {
+      Notification.requestPermission();
     }
   };
-
 
   const clickCancel = () => {
     setShowTimerModal(false);
