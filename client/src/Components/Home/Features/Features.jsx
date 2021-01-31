479b98bce0a42b3c58411739b92110cf75edc713
AmanKuvera-Dev <65402367+AmanKuvera-Dev@users.noreply.github.com>
2021-01-31 23:21:52 +0530

    updated Features Section

--- client/src/Components/Home/Features/Features.jsx
+++ client/src/Components/Home/Features/Features.jsx
@@ -1,10 +1,12 @@
 import styles from './Features.module.css';
 import { Container, Row, Column } from './Bootstrap';
-import HistoryFeature from './../../../images/HistoryFeature.svg';
+import PdfFeature from './../../../images/PdfFeature.svg';
 import voiceText from './../../../images/voiceText.svg';
 import timeQueue from './../../../images/timeQueue.svg';
 import timerDoubt from './../../../images/timerDoubt.svg';
 import oneClick from './../../../images/oneClick.svg';
+import privacy from './../../../images/privacy.svg';
+import realtime from './../../../images/realtime.svg';
 
 export default function Features() {
   return (
@@ -13,36 +15,48 @@ export default function Features() {
 
       <Container>
         <Row>
-          <Column size='md-4'>
+          <Column size='md-3'>
             <div className={styles.item}>
-              <img className={styles.image} src={HistoryFeature} alt='icon' />
-              <h5 className={styles.info}>History record of answered questions</h5>
+              <img className={styles.image} src={PdfFeature} alt='icon' />
+              <h5 className={styles.info}>Download PDF of answered questions</h5>
             </div>
           </Column>
-          <Column size='md-4'>
+          <Column size='md-3'>
             <div className={styles.item}>
               <img className={styles.image} src={voiceText} alt='icon' />
               <h5 className={styles.info}>Voice to text conversion of answers</h5>
             </div>
           </Column>
-          <Column size='md-4'>
+          <Column size='md-3'>
             <div className={styles.item}>
               <img className={styles.image} src={timeQueue} alt='icon' />
               <h5 className={styles.info}>Timestamp based questions queue</h5>
             </div>
           </Column>
-        </Row>
-        <Row>
-          <Column size='md-6'>
+          <Column size='md-3'>
             <div className={styles.item}>
               <img className={styles.image} src={timerDoubt} alt='icon' />
               <h5 className={styles.info}>Set timer for doubt clearance session</h5>
             </div>
           </Column>
-          <Column size='md-6'>
+        </Row>
+        <Row>
+          <Column size='md-4'>
+            <div className={styles.item}>
+              <img className={styles.image} src={privacy} alt='icon' />
+              <h5 className={styles.info}>Privacy and Security</h5>
+            </div>
+          </Column>
+          <Column size='md-4'>
             <div className={styles.item}>
               <img className={styles.image} src={oneClick} alt='icon' />
               <h5 className={styles.info}>One click session commencement</h5>
+            </div>
+          </Column>
+          <Column size='md-4'>
+            <div className={styles.item}>
+              <img className={styles.image} src={realtime} alt='icon' />
+              <h5 className={styles.info}>Realtime Dashboard Updates</h5>
             </div>
           </Column>
         </Row>
